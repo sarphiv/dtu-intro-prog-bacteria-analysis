f21b3764d1fe59c4d5afac0602eee9d6d0e9a60c
sarphiv <32750711+sarphiv@users.noreply.github.com>
2020-11-05 19:01:43 +0100

    Finished plots module and added more functionality to statistics module

--- lib/plot.py
+++ lib/plot.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
-from lib.statistics import dataStatistics
+from lib.statistics import dataStatistics, get_temperature, get_growth_rate
 from lib.data import bacteria_species
 
 
+#Define axis label style for subplots
 axis_label_style = { "fontsize": 12, "fontweight": "bold" }
 
 
 def draw_bar_plot(fig, key_value):
+    """
+    Draw a bar plot defined by the keys as categories and values as heights.
+    """
+
     #Draw horizontal grid lines
     fig.yaxis.grid(zorder=0)
     
@@ -25,24 +30,70 @@ def draw_bar_plot(fig, key_value):
     #Rotate x-axis tick labels
     for label in fig.get_xticklabels():
         label.set_fontsize(9)
-        label.set_rotation(16)
-        label.set_ha("right")
+        label.set_rotation(12)
 
 
-def draw_line_graph(fig, x, y, color):
-    pass
+def draw_line_graph(fig, x, y, name):
+    """
+    Draw a combined point and dashed line graph based on the given points.
+    The graph is labeled.
+    """
+    
+    #Draw grid lines
+    fig.grid(zorder=0)
+    
+    #Draw points and grey lines
+    fig.plot(x, y, "o", label=name, zorder=2)
+    fig.plot(x, y, "k--", label=None, zorder=1)
+    
+    #Set axis limits
+    fig.set_xlim(0, 60)
+    fig.set_ylim(0)
+    
+    #Display legend
+    fig.legend()
+    
+    #Set axis labels
+    fig.set_title("Growth rate based on temperature", **axis_label_style)
+    fig.set_xlabel("Temperature", **axis_label_style)
+    fig.set_ylabel("Growth rate", **axis_label_style)
+
 
 
 def dataPlot(data):
+    """
+    Shows a GUI with number of entries for each species and a growth-temperature plot for each species.
+    NOTE: Blocks thread while the GUI is open.
+    """
+
+    #Create subplots and setup size
     fig, (fig_num, fig_growth) = plt.subplots(1, 2)
     fig.subplots_adjust(bottom=0.24)
     fig.set_size_inches(13, 6)
 
+
+    #Get number of entries for each species and transform to key by string name
     species_num = dataStatistics(data, "rows by species")
     species_num = { bacteria_species[s]: num for s, num in species_num.items() }
 
-    draw_bar_plot(fig_num, species_num)
-    #fig_num.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
-    #fig_num.axis([0, 6, 0, 20])
-    plt.show()
+    #Get temperature and growth data, then sort by temperature
+    #NOTE: Sorts by getting a sorted array of indexes for the temperature column,
+    # then accesses the original array in the order of the sorted indexes.
+    species_temp_grow = dataStatistics(data, "temperature and growth rate by species")
+    species_temp_grow = { s: e[e[:, 0].argsort()] for s, e in species_temp_grow.items()}
 
+
+    #Draw number plot
+    draw_bar_plot(fig_num, species_num)
+    
+    #Draw growth-temperature plots for each species
+    for species, entries in species_temp_grow.items():
+        draw_line_graph(fig_growth, 
+                        x=get_temperature(entries),
+                        y=get_growth_rate(entries), 
+                        name=bacteria_species[species])
+
+
+    #Show finished plot
+    #NOTE: Blocks thread
+    plt.show()
--- lib/statistics.py
+++ lib/statistics.py
@@ -11,34 +11,66 @@ get_species     = lambda data: data[:, 2]
 #Dictionary mapping from statistic operation name to function
 statistic_functions = {
     #Get mean of temperature
-    "mean temperature":      lambda data: get_temperature(data).mean(),
+    "mean temperature":       lambda data: 
+        get_temperature(data).mean(),
+
     #Get mean of growth rate
-    "mean growth rate":      lambda data: get_growth_rate(data).mean(),
+    "mean growth rate":       lambda data: 
+        get_growth_rate(data).mean(),
+
     #Get standard deviation of temperature
-    "std temperature":       lambda data: get_temperature(data).std(),
+    "std temperature":        lambda data: 
+        get_temperature(data).std(),
+
     #Get standard deviation of growth rate
-    "std growth rate":       lambda data: get_growth_rate(data).std(),
+    "std growth rate":        lambda data: 
+        get_growth_rate(data).std(),
+
     #Get length of first dimension (rows) of data
-    "rows":                  lambda data: data.shape[0],
+    "rows":                   lambda data: data.shape[0],
+
     #Get mean growth rate of rows with temperature below 20
-    "mean cold growth rate": lambda data: 
+    "mean cold growth rate":  lambda data: 
         dataStatistics(
             data[get_temperature(data) < 20],
             "mean growth rate"
         ),
+
     #Get mean growth rate of rows with temperature above 50
-    "mean hot growth rate":  lambda data: 
+    "mean hot growth rate":   lambda data: 
         dataStatistics(
             data[get_temperature(data) > 50],
             "mean growth rate"
         ),
-    #Number of each species as a dictionary {species: number}
-    "rows by species":       lambda data: { 
-            species: dataStatistics(
-                data[get_species(data) == species], 
-                "rows") 
+
+    #Data keyed by species
+    "data by species":        lambda data: { 
+            species: data[get_species(data) == species]
             for species in bacteria_species.keys()
-        }
+        },
+    
+    #Amount of each species keyed by species {species: number}
+    "rows by species":        lambda data: { 
+            species: dataStatistics(entries, "rows")
+            for species, entries in dataStatistics(data, "data by species").items()
+        },
+
+    #Temperature keyed by species
+    "temperature by species": lambda data: { 
+            species: get_temperature(entries)
+            for species, entries in dataStatistics(data, "data by species").items()
+        },
+
+    #Growth rate keyed by species
+    "growth rate by species": lambda data: { 
+            species: get_growth_rate(entries)
+            for species, entries in dataStatistics(data, "data by species").items()
+        },
+    #Temperature and growth rate keyed by species
+    "temperature and growth rate by species": lambda data: { 
+            species: entries[:, 0:2]
+            for species, entries in dataStatistics(data, "data by species").items()
+        },
 }
 
 
@@ -53,7 +85,11 @@ def dataStatistics(data, statistic):
         "rows",
         "mean cold growth rate",
         "mean hot growth rate",
-        "rows by species"
+        "data by species",
+        "rows by species",
+        "temperature by species",
+        "growth rate by species",
+        "temperature and growth rate by species"
     """
     #Get appropriate statistic function with string.
     # Apply function on data entries
