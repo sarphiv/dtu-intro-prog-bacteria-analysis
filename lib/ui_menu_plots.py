566fc67d58d8017f31be6ba25eca3a89c3f366c7
sarphiv <32750711+sarphiv@users.noreply.github.com>
2020-11-07 14:32:52 +0100

    Documented everything except ui_filters

--- lib/ui_menu_plots.py
+++ lib/ui_menu_plots.py
@@ -3,8 +3,15 @@ from lib.plot import dataPlot
 
 
 def display_plots(state):
+    """
+    Opens a GUI to show the filtered data with plots.
+    Does not continue if there is no data available.
+    """
+
+    #If no data is unavailable, inform user and return
     if inform_if_data_unavailable(state.filtered_data):
         return
 
+    #Show GUI with plots
     print("Close plots window to continue...", end="\n\n")
     dataPlot(state.filtered_data)
